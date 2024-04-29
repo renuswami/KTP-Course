25b093b663479f3cb6185a389bdf4031a5cdcfb0
renuswami <renuswami2001@gmail.com>
2024-04-29 20:59:38 +0530

    Grade Calculator Mini App Executed!

--- ProgrammingFundamentals/Ep3/FactorialCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorialCalculator/src/Task.kt
@@ -1,15 +1,18 @@
 import com.faangx.ktp.basics.FactorialCalculatorMiniApp
 
 fun factorialOf(x: Long): Long {
-    TODO()
+    var fact = 1L
+    for (i in 1..x){
+     fact = i*x
+    }
+    return fact
 }
 
 fun permutationsOf(n: Long, r: Long): Long {
-    TODO()
-}
+    return factorialOf(n) / factorialOf(n - r)}
 
 fun combinationsOf(n: Long, r: Long): Long {
-    TODO()
+    return factorialOf(n) / (factorialOf(r) * factorialOf(n - r))
 }
 
 fun main() {
