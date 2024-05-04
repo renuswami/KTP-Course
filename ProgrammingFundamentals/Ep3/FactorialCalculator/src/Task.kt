33c839fc2406874087a0cb3794119f919edfc7bd
renuswami <renuswami2001@gmail.com>
2024-05-04 23:36:33 +0530

    Factorial Calculator Mini App Executed!

--- ProgrammingFundamentals/Ep3/FactorialCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorialCalculator/src/Task.kt
@@ -3,16 +3,21 @@ import com.faangx.ktp.basics.FactorialCalculatorMiniApp
 fun factorialOf(x: Long): Long {
     var fact = 1L
     for (i in 1..x){
-     fact = i*x
+     fact *= i
     }
     return fact
 }
 
 fun permutationsOf(n: Long, r: Long): Long {
-    return factorialOf(n) / factorialOf(n - r)}
+    var permutation = 1L
+    for(i in (n-r+1)..n){
+        permutation *= i
+    }
+    return permutation
+}
 
 fun combinationsOf(n: Long, r: Long): Long {
-    return factorialOf(n) / (factorialOf(r) * factorialOf(n - r))
+    return permutationsOf(n, r) / factorialOf(r)
 }
 
 fun main() {
