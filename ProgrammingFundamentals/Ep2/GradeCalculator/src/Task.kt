cc858600d33b7d09e3bb93df03393cce14d48071
renuswami <renuswami2001@gmail.com>
2024-05-04 22:01:52 +0530

    Factor Calculator Mini App Executed!

--- ProgrammingFundamentals/Ep2/GradeCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep2/GradeCalculator/src/Task.kt
@@ -4,6 +4,7 @@ fun gradeFor(m1: Int, m2: Int, m3: Int, m4: Int, m5: Int): String {
     val average = (m1 + m2 + m3 + m4 + m5) / 50f
     return when (average) {
         in 9f..10f -> "A+"
+
         in 8f..9f -> "A"
         in 7f..8f -> "B"
         in 6f..7f -> "C"
