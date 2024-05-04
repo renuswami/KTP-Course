c28885331f3a000328221401eed1612eed315e35
renuswami <renuswami2001@gmail.com>
2024-05-04 22:01:52 +0530

    Factor Calculator Mini App Executed!

--- ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
@@ -1,11 +1,27 @@
 import com.faangx.ktp.basics.FactorCalculatorMiniApp
 
 fun printFactorsOf(num: Int) {
-    TODO()
+    for (i in 1..num){
+        if(num % i == 0){
+            print("$i, ")
+        }
+    }
 }
 
 fun isPrime(num: Int): Boolean {
-    TODO()
+    var flag = false
+
+    if(num == 0 || num ==1){
+        flag = true
+    }else{
+        for(i in 2 .. num/2){
+
+            if (num % i == 0){
+                flag = true
+            }
+        }
+    }
+    return !flag
 }
 
 fun main() {
