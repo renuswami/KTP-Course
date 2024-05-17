c1fc84f4d95882ab3a576692946895693efe16ee
renuswami <renuswami2001@gmail.com>
2024-05-17 21:25:11 +0530

    initial commit for update

--- ProgrammingFundamentals/Ep3.Ext/Pattern1/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern1/src/Task.kt
@@ -1,9 +1,9 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-
+    repeat(lines){i ->
+    }
 }
-
 
 fun main() {
     PatternMiniApp("P1", ::printPattern)
