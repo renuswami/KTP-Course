0165250adf1eafb94aa94a7df1c6d7779a9b8121
renuswami <renuswami2001@gmail.com>
2024-05-20 21:00:18 +0530

    Ep3 Ext 5 patterns

--- ProgrammingFundamentals/Ep3.Ext/Pattern3/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern3/src/Task.kt
@@ -1,7 +1,22 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    repeat(lines) { i ->
+        repeat(lines - i - 1) {
+            print(char2)
+        }
+        print(char1)
+        repeat(2 * i-1) {
+            print(char2)
+        }
+        if (i != 0) { // To avoid printing an extra character in the first row
+            print(char1)
+        }
+        repeat(lines - i - 1) {
+            print(char2)
+        }
+        println()
+    }
 }
 
 fun main() {
