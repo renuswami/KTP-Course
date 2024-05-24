a118f5a4085523891cb597b8ffa38cc12eb50aed
renuswami <renuswami2001@gmail.com>
2024-05-24 22:39:10 +0530

    Ep3 Ext Binomial expansion

--- ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
@@ -1,21 +1,64 @@
-import com.faangx.ktp.ext.BinomialExpansionMiniApp
-
 fun printBinomialExpansion(n: Int) {
-    TODO()
+    if (n == 0) {
+        println("1") // Special case for n = 0
+        return
+    }
+
+    if (n == 1) {
+        println("a + b") // Special case for n = 1
+        return
+    }
+
+    for (i in 0..n) {
+        val coeff = combinationsOf(n, i)
+        val powerOfA = n - i
+        val powerOfB = i
+
+        if (coeff != 1) print("$coeff")
+
+        if (powerOfA > 0) {
+            if (coeff > 1) {
+                if (powerOfA == 1) print("a") else print("a^$powerOfA")
+            } else {
+                print("a")
+            }
+        }
+
+        if (powerOfA > 0 && powerOfB > 0) print("*")
+
+        if (powerOfB > 0) {
+            if (coeff > 1) {
+                if (powerOfB == 1) print("b") else print("b^$powerOfB")
+            } else {
+                print("b")
+            }
+        }
+
+        if (i < n) print(" + ")
+    }
+    println()
 }
 
 fun factorialOf(x: Int): Int {
-    TODO()
+    var fact = 1
+    for (i in 1..x) {
+        fact *= i
+    }
+    return fact
 }
 
 fun permutationsOf(n: Int, r: Int): Int {
-    TODO()
+    var permutation = 1
+    for (i in (n - r + 1)..n) {
+        permutation *= i
+    }
+    return permutation
 }
 
 fun combinationsOf(n: Int, r: Int): Int {
-    TODO()
+    return permutationsOf(n, r) / factorialOf(r)
 }
 
 fun main() {
-    BinomialExpansionMiniApp(::printBinomialExpansion)
+    printBinomialExpansion(1) // Change this value for different n
 }
--- ProgrammingFundamentals/Ep3.Ext/Pattern1/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern1/src/Task.kt
@@ -2,6 +2,14 @@ import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
     repeat(lines){i ->
+        repeat(lines){j ->
+            if (i == j){
+                print(char1)
+            }else{
+                print(char2)
+            }
+        }
+        println()
     }
 }
 
